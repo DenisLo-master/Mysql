9c4ae756257e50831f00680f0b7898b159c540c9
Denis <denis.lkg@gmial.com>
2020-07-18 18:54:41 +0300

    HW

---
+++ s1.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users(id INT,
+name VARCHAR(255) COMMENT 'Название') COMMENT 'Таблица пользователей';
+
+-- команда в терминале
+-- mysqldump example > sample.sql
+
+CREATE DATABASE IF NOT EXISTS sample;
+
+-- команда в терминале
+-- mysql sample < sample.sql;
+
+-- mysqldump --where='0 limit 100' mysql help_keyword > keyword.sql
+
