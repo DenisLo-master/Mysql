bb709e61eb1170d1946a98417400edcb32452863
Denis <denis.lkg@gmial.com>
2020-07-19 17:04:08 +0300

    HW les2

--- s1.sql
+++ s1.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE IF NOT EXISTS example;
+USE example;
 DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users(id INT,
 name VARCHAR(255) COMMENT 'Название') COMMENT 'Таблица пользователей';
@@ -9,5 +11,12 @@ CREATE DATABASE IF NOT EXISTS sample;
 
 -- команда в терминале
 -- mysql sample < sample.sql;
--- mysqldump --where='0 limit 100' mysql help_keyword > keyword.sql
 
+
+
+-- mysqldump  mysql help_keyword --where='TRUE ORDER BY help_keyword_id LIMIT 100' > keyword.sql
+CREATE DATABASE IF NOT EXISTS keyword;
+USE keyword;
+-- команда в терминале
+-- mysql keyword < keyword.sql
+-- ошибка ERROR 3723 (HY000) at line 25: The table 'help_keyword' may not be created in the reserved tablespace 'mysql'.
