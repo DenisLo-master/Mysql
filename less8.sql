93966be1881ecdf992a4bb0dd39e60f6f5917bca
Denis <denis.lkg@gmial.com>
2020-08-26 02:50:04 +0300

    HW les8 update

--- less8.sql
+++ less8.sql
@@ -30,7 +30,7 @@ SELECT * FROM likes WHERE id=2;
 SELECT * FROM messages WHERE id=26;
 SELECT * FROM users WHERE id=18;
 
--- решение
+-- НЕ  ПРАВИЛЬНОЕ РЕШЕНИЕ
 SELECT SUM(count_likes) AS total_likes FROM (
 	SELECT name, birthday, COUNT(liked_id) AS count_likes FROM	(	
 		(SELECT DISTINCT CONCAT(u.first_name, ' ', u.last_name) AS name, pr.birthday AS birthday, t.name AS target, l.target_id AS target_id, l.id AS liked_id FROM likes AS l
@@ -77,6 +77,49 @@ SELECT SUM(count_likes) AS total_likes FROM (
 	GROUP BY name , birthday ORDER BY birthday DESC LIMIT 10
 ) AS total_sum;
 
+-- правильное решение 
+SELECT * FROM target_types;
+SELECT * FROM posts;
+
+-- проверка posts
+SELECT * FROM likes WHERE target_id=27;
+SELECT * FROM posts WHERE id=27;
+SELECT * FROM users WHERE id=66;
+SELECT * FROM messages WHERE id=7;
+
+-- проверка messages
+SELECT * FROM likes WHERE target_id=70;
+SELECT * FROM likes WHERE target_id=71;
+SELECT * FROM likes WHERE target_id=72;
+SELECT * FROM likes WHERE target_id=89;
+SELECT * FROM messages WHERE id=70;
+SELECT * FROM messages WHERE id=71;
+SELECT * FROM messages WHERE id=72;
+SELECT * FROM users WHERE id=66;
+
+SELECT SUM(total_liked) AS total_liked FROM (
+	SELECT u.first_name , u.last_name , pr.birthday ,
+		COUNT(DISTINCT l.id) AS total_liked
+		FROM likes AS l
+			INNER JOIN target_types AS t
+				ON l.target_type_id = t.id
+			LEFT JOIN posts AS p
+				ON l.target_id  = p.id AND t.name = 'posts'
+			LEFT JOIN media AS md
+				ON l.target_id  = md.id AND t.name = 'media'
+			LEFT JOIN users AS ul
+				ON l.target_id  = ul.id AND t.name = 'users'
+			LEFT JOIN messages AS m
+				ON l.target_id  = m.id AND t.name = 'messages'
+			RIGHT JOIN users AS u
+				ON u.id = p.user_id OR u.id = md.user_id OR u.id = ul.id OR u.id = m.from_user_id 
+			RIGHT JOIN profiles AS pr 
+				ON u.id = pr.user_id
+	GROUP BY u.first_name, u.last_name, pr.birthday ORDER BY pr.birthday DESC , total_liked LIMIT 10) AS liked;
+
+
+
+
 
 /* 3. Найти 10 пользователей, которые проявляют наименьшую активность в использовании социальной сети
 (критерии активности необходимо определить самостоятельно).*/
@@ -85,6 +128,7 @@ SELECT * FROM messages;
 SELECT * FROM friendship;
 DESC friendship;
 
+-- НЕ  ПРАВИЛЬНОЕ РЕШЕНИЕ
 -- критерий активности: сумма действий в каждом модуле и последняя активность по дате и времени
 SELECT name, birthday ,SUM(count_act) AS total_activities, MAX(last_updated) FROM (
 	(SELECT DISTINCT CONCAT(u.first_name, ' ', u.last_name) AS name, pr.birthday AS birthday, COUNT(p.id) AS count_act, MAX(p.updated_at) AS last_updated FROM users AS u
@@ -125,6 +169,28 @@ GROUP BY name, birthday ORDER BY total_activities, MIN(last_updated) LIMIT 10;
 
 
 		
+-- правильное решение
+
+SELECT CONCAT(u.first_name, ' ', u.last_name) AS name, 
+	COUNT(DISTINCT m.id) + 
+	COUNT(DISTINCT l.id) + 
+	COUNT(DISTINCT p.id) + 
+	COUNT(DISTINCT md.id) AS activities
+	FROM users AS u
+		LEFT JOIN messages AS m 
+			ON u.id = m.from_user_id 
+		LEFT JOIN media AS md
+			ON u.id = md.user_id 
+		LEFT JOIN posts AS p 
+			ON u.id = p.user_id 
+		LEFT JOIN likes AS l
+			ON u.id = l.user_id
+		LEFT JOIN friendship AS f
+			ON u.id = f.user_id
+GROUP BY name ORDER BY activities DESC , name ;
+
+
+
 	
 	
 	
